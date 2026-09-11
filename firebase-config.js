c2c66d7013fecb27b24f0dad6540b87acbdf28fd
sgdurga18-coder <sgdurga18@gmail.com>
2026-09-11 17:19:17 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyChqNzZERTyeIAyDr1a2rCZL35jEM65Cvk",
+  authDomain: "nexcalc-90ee5.firebaseapp.com",
+  projectId: "nexcalc-90ee5",
+  storageBucket: "nexcalc-90ee5.firebasestorage.app",
+  messagingSenderId: "372416722729",
+  appId: "1:372416722729:web:8c99e5007a3105508ce938",
+  measurementId: "G-94LLC7230Q"
+};
